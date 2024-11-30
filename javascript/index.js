6776f3316909d0892c73885d1a72ac95f32f5e46
lavanya-vanwyk <lavanyavanwyk1@gmail.com>
2024-12-01 00:55:55 +0200

    Updated updateWeatherInfo function to display realtime day and time. Created formatDate function to handle the format of the date and time.

--- javascript/index.js
+++ javascript/index.js
@@ -6,13 +6,41 @@ function updateWeatherInfo(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
-  console.log(response.data.wind.speed);
-  
+  let timeElement = document.querySelector("#current-time");
+  let date = new Date(response.data.time *1000);
+
+  timeElement.innerHTML = formatDate(date);
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+}
+function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+
+    let day = days[date.getDay()];
+  
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+
+return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
