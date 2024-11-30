b7b57f3714f5dc94ec9ffafbeb8abb17d7c78027
lavanya-vanwyk <lavanyavanwyk1@gmail.com>
2024-12-01 00:38:56 +0200

    Updated updateWeatherInfo function to display realtime humidity and wind speed information. Removed formatDate function.

--- javascript/index.js
+++ javascript/index.js
@@ -1,41 +1,16 @@
 
-
-// function formatDate(date) {
-//   let minutes = date.getMinutes();
-//   let hours = date.getHours();
-//   let day = date.getDay();
-
-//   if (minutes < 10) {
-//     minutes = `0${minutes}`;
-//   }
-
-//   if (hours < 10) {
-//     hours = `0${hours}`;
-//   }
-
-//   let days = [
-//     "Sunday",
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday"
-//   ];
-
-//   let formattedDay = days[day];
-//   return `${formattedDay} ${hours}:${minutes}`;
-// }
-
-// let currentDateElement = document.querySelector("#current-date");
-// let currentDate = new Date();
-
-// currentDateElement.innerHTML = formatDate(currentDate);
 function updateWeatherInfo(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
-  let cityElement = document.querySelector("#current-city"); 
+  let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  console.log(response.data.wind.speed);
   
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
 }
