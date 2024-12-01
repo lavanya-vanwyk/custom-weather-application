2f45d4e2319750c377b0ac00a3b42272898c7274
lavanya-vanwyk <lavanyavanwyk1@gmail.com>
2024-12-01 23:45:02 +0200

    HTML forecast elements created through JS

--- javascript/index.js
+++ javascript/index.js
@@ -59,6 +59,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="forcast-date">
+            <div class="forecast-day">${day}</div>
+            <div class="forecast-icon">🌤️</div>
+            
+            <div class="forecast-temperatures">
+              <div class="forecast-temperature"><strong>15°</strong></div>
+              <div class="forecast-temperature">9°</div>  
+          </div>
+          </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+searchCity("Paris");
+displayForecast();
